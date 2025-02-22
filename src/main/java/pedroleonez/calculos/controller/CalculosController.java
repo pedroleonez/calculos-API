dc959a216601a6790ecf348cdb7585e5c13107f1
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-02-22 19:56:11 -0300

    Update CalculosController.java

--- src/main/java/pedroleonez/calculos/controller/CalculosController.java
+++ src/main/java/pedroleonez/calculos/controller/CalculosController.java
@@ -1,22 +1,32 @@
 package pedroleonez.calculos.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pedroleonez.calculos.entity.Entrada;
 import pedroleonez.calculos.entity.Resultado;
+import pedroleonez.calculos.service.CalculosService;
 
 @RestController
 @RequestMapping("/calculos")
 public class CalculosController {
 
-    public ResponseEntity<Resultado> somar(Entrada entrada) {
+    private final CalculosService calculosService;
+
+    public CalculosController(CalculosService calculosService) {
+        this.calculosService = calculosService;
+    }
+
+    @GetMapping("/calcular")
+    public ResponseEntity<Resultado> calcular(@RequestBody Entrada entrada) {
         try {
-            Resultado resultado = new Resultado();
-            resultado.setSoma(entrada.getLista().stream().mapToInt(Integer::intValue).sum());
-            return ResponseEntity.ok(resultado);
+            Resultado resultado = this.calculosService.calcular(entrada);
+            return new ResponseEntity<>(resultado, HttpStatus.OK);
         } catch (Exception e) {
-            return ResponseEntity.badRequest().build();
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
     }
 }
